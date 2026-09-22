d1d015e6be5715dc139dceeb8b120556a38cadb7
elyttle <elyttle1@students.towson.edu>
2026-09-21 23:59:05 -0400

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
     let x =10;
+    let a = true;
+    let b = false;
+
     println!("Hello, world!");
-    println!("x ix {}",x)
+
+    println!("x ix {}",x);
+
+    println!("a is {} and b is {}",a,b);
+    println!("Not a is {}",!a);
+    println!("a AND b is {}",a & b);
+    println!("a OR b is {}",a|b);
+    println!("a XOR b is {}",a^b);
 }
